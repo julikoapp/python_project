0a2fe98a4690be240f19f4fc322b62c6667b1c71
Julia <yula.barko@gmail.com>
2020-07-20 10:49:25 +0600

    add program Bookstore, that was created using Tkinter and SQLite3

---
+++ old-book-store/data_func.py
@@ -0,0 +1,55 @@
+import sqlite3
+
+
+class Database:
+
+    def __init__(self, db):
+        self.db = db
+        self.conn = sqlite3.connect(self.db)
+        self.curs = self.conn.cursor()
+        self.curs.execute(
+            "CREATE TABLE IF NOT EXISTS bookstore(id INTEGER PRIMARY KEY,title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+        self.conn.commit()
+
+    def view_all_books(self):
+        self.curs.execute("SELECT * FROM bookstore")
+        rows = self.curs.fetchall()
+        self.conn.commit()
+        # self.conn.close()
+        return rows
+
+    def add_book(self, title, author, year, isbn):
+        self.curs.execute("INSERT INTO bookstore VALUES(NULL, ?, ?,?, ?)", (title, author, year, isbn))
+        self.conn.commit()
+        # self.conn.close()
+
+    def find_book(self, name="", author="", year="", isbn=""):
+        self.curs.execute("SELECT * FROM bookstore WHERE title=?OR author=? OR year=? or isbn=?",
+                          (name, author, year, isbn))
+        rows = self.curs.fetchall()
+        self.conn.commit()
+        # self.conn.close()
+        return rows
+
+    def delete_book(self, id):
+        self.curs.execute("DELETE FROM bookstore WHERE id=?", (id,))
+        self.conn.commit()
+        # self.conn.close()
+
+    def update(self, id, title, author, year, isbn):
+        self.curs.execute("UPDATE bookstore SET title=?, author=?, year=?, isbn=? WHERE id=?",
+                          (id, title, author, year, isbn))
+        self.conn.commit()
+        # self.conn.close()
+
+    def delete_table(self):
+        self.curs.execute("DROP TABLE IF EXISTS bookstore")
+        self.conn.commit()
+        # self.conn.close()
+
+    def __del__(self):
+        # when script executes
+        self.conn.close()
+
+# print(view_all_books())
+# print(find_book("Red"))
